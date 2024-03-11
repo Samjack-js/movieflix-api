6a88fee99e814259d2cc2e1d529c3516fe975579
Samjack-js <gabrieljaquim@hotmail.com>
2024-03-11 21:37:33 +0000

    configurações ESlint

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,1 @@
-const http = require('http');
-
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-
-    if(req.url === '/'){
-        res.statusCode = 200;
-        res.end('Home Page');
-    }else if(req.url === '/about'){
-        res.statusCode = 200;
-        res.end('About Page');
-    }
-});
-
-server.listen(3000);
-
-console.log('Server running at http://127.0.0.1:3000/');
+const teste = 'test'
