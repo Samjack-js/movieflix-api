9d3e6680861c78e3491369a6d6640681cc18e91c
Samjack-js <gabrieljaquim@hotmail.com>
2024-03-11 21:45:05 +0000

    configurações express

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,12 @@
-const teste = 'test'
+import express from 'express'
+
+const port = 3000
+const app = express()
+
+app.get('/movies', (req, res) => {
+	res.send('Lista de filmes')
+})
+
+app.listen(port, () => {
+	console.log(`Example app listening at http://localhost:${port}`)
+})
